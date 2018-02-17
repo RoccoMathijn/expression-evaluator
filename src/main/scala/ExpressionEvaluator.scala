80d7b2a278f67de22bb7dfee7baeb387342ef5ea
rmathijn <rocco.mathijnandela@dhl.com>
2018-02-17 13:21:18 +0100

    Simple expression evaluator

---
+++ src/main/scala/ExpressionEvaluator.scala
@@ -0,0 +1,98 @@
+object ExpressionEvaluator {
+
+  def evaluate(expr: Expr): Answer = expr match {
+    case IntVal(n) => IntAnswer(Some(n))
+    case app@App(_: ArithmeticOperator, _, _) if valid(app) => IntAnswer(Some(applyArithmetic(app)))
+    case App(_: ArithmeticOperator, _, _) => IntAnswer(None)
+    case app@App(_: ComparisonOperator, _, _) if valid(app) => BooleanAnswer(Some(applyComparison(app)))
+    case App(_: ComparisonOperator, _, _) => BooleanAnswer(None)
+    case app@App(_: BooleanOperator, _, _) if valid(app) => BooleanAnswer(Some(applyLogicalComparison(app)))
+    case App(_: BooleanOperator, _, _) => BooleanAnswer(None)
+  }
+
+  def applyArithmetic(app: App): Int = app match {
+    case App(Add, IntVal(left), IntVal(right)) => left + right
+    case App(Sub, IntVal(left), IntVal(right)) => left - right
+    case App(Mul, IntVal(left), IntVal(right)) => left * right
+    case App(Div, IntVal(left), IntVal(right)) => left / right
+    case _ => throw new Error("This shouldn't happen!")
+  }
+
+  def applyComparison(app: App): Boolean = app match {
+    case App(_, _: BoolVal, _: IntVal) => throw new Error("Incomparable types!")
+    case App(_, _: IntVal, _: BoolVal) => throw new Error("Incomparable types!")
+    case App(Lt, BoolVal(left), BoolVal(right)) => left < right
+    case App(LTe, BoolVal(left), BoolVal(right)) => left <= right
+    case App(GTe, BoolVal(left), BoolVal(right)) => left >= right
+    case App(Gt, BoolVal(left), BoolVal(right)) => left > right
+    case App(Ne, BoolVal(left), BoolVal(right)) => left != right
+    case App(Eq, BoolVal(left), BoolVal(right)) => left == right
+    case App(Lt, IntVal(left), IntVal(right)) => left < right
+    case App(LTe, IntVal(left), IntVal(right)) => left <= right
+    case App(GTe, IntVal(left), IntVal(right)) => left >= right
+    case App(Gt, IntVal(left), IntVal(right)) => left > right
+    case App(Ne, IntVal(left), IntVal(right)) => left != right
+    case App(Eq, IntVal(left), IntVal(right)) => left == right
+    case _ => throw new Error("This shouldn't happen!")
+  }
+
+  def applyLogicalComparison(app: App): Boolean = app match {
+    case App(Or, BoolVal(left), BoolVal(right)) => left || right
+    case App(And, BoolVal(left), BoolVal(right)) => left && right
+    case App(Not, BoolVal(left), BoolVal(right)) => left != right
+    case _ => throw new Error("This shouldn't happen!")
+  }
+
+  def valid(app: App): Boolean = app match {
+    case App(Div, _, IntVal(right)) if right == 0 => false
+    case _ => true
+  }
+
+  trait Operator
+
+  trait ArithmeticOperator extends Operator
+
+  trait ComparisonOperator extends Operator
+
+  trait BooleanOperator extends Operator
+
+  trait Expr
+
+  trait Answer
+
+  case class IntVal(n: Int) extends Expr
+
+  case class BoolVal(n: Boolean) extends Expr
+
+  case class App(operator: Operator, left: Expr, right: Expr) extends Expr
+
+  case class IntAnswer(value: Option[Int]) extends Answer
+
+  case class BooleanAnswer(value: Option[Boolean]) extends Answer
+
+  case object Add extends ArithmeticOperator
+
+  case object Sub extends ArithmeticOperator
+
+  case object Mul extends ArithmeticOperator
+
+  case object Div extends ArithmeticOperator
+
+  case object Lt extends ComparisonOperator
+
+  case object LTe extends ComparisonOperator
+
+  case object GTe extends ComparisonOperator
+
+  case object Gt extends ComparisonOperator
+
+  case object Ne extends ComparisonOperator
+
+  case object Eq extends ComparisonOperator
+
+  case object Or extends BooleanOperator
+
+  case object And extends BooleanOperator
+
+  case object Not extends BooleanOperator
+}
---
+++ src/test/scala/ExpressionEvaluatorTest.scala
@@ -0,0 +1,221 @@
+import ExpressionEvaluator.{ Eq, Gt, Ne, _ }
+import org.scalatest.{ Matchers, WordSpec }
+
+class ExpressionEvaluatorTest extends WordSpec with Matchers {
+  "The Expression evaluator" can {
+    "do basic arithmetic operations on integers" should {
+      "add" in {
+        val expression = App(Add, IntVal(5), IntVal(3))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (IntAnswer(Some(8)))
+      }
+
+      "subtract" in {
+        val expression = App(Sub, IntVal(5), IntVal(3))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (IntAnswer(Some(2)))
+      }
+
+      "multiply" in {
+        val expression = App(Mul, IntVal(5), IntVal(3))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (IntAnswer(Some(15)))
+      }
+
+      "divide" in {
+        val expression = App(Div, IntVal(10), IntVal(5))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (IntAnswer(Some(2)))
+      }
+
+      "return None when dividing by zero" in {
+        val expression = App(Div, IntVal(10), IntVal(0))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (IntAnswer(None))
+      }
+    }
+
+    "do comparison operations on integers" should {
+      "Lt positive" in {
+        val expression = App(Lt, IntVal(1), IntVal(2))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(true)))
+      }
+      "Lt negative" in {
+        val expression = App(Lt, IntVal(2), IntVal(1))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(false)))
+      }
+      "LTe positive" in {
+        val expression = App(LTe, IntVal(1), IntVal(1))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(true)))
+      }
+      "LTe negative" in {
+        val expression = App(LTe, IntVal(2), IntVal(1))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(false)))
+      }
+      "GTe positive" in {
+        val expression = App(GTe, IntVal(5), IntVal(3))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(true)))
+      }
+      "GTe negative" in {
+        val expression = App(GTe, IntVal(3), IntVal(5))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(false)))
+      }
+      "Gt positive" in {
+        val expression = App(Gt, IntVal(5), IntVal(3))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(true)))
+      }
+      "Gt negative" in {
+        val expression = App(Gt, IntVal(3), IntVal(5))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(false)))
+      }
+      "Ne positive" in {
+        val expression = App(Ne, IntVal(5), IntVal(3))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(true)))
+      }
+      "Ne negative" in {
+        val expression = App(Ne, IntVal(5), IntVal(5))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(false)))
+      }
+      "Eq positive" in {
+        val expression = App(Eq, IntVal(5), IntVal(5))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(true)))
+      }
+      "Eq negative" in {
+        val expression = App(Eq, IntVal(5), IntVal(3))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(false)))
+      }
+    }
+    "do comparison operations on booleans" should {
+      "Lt positive" in {
+        val expression = App(Lt, BoolVal(false), BoolVal(true))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(true)))
+      }
+      "Lt negative" in {
+        val expression = App(Lt, BoolVal(true), BoolVal(false))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(false)))
+      }
+      "LTe positive" in {
+        val expression = App(LTe, BoolVal(true), BoolVal(true))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(true)))
+      }
+      "LTe negative" in {
+        val expression = App(LTe, BoolVal(true), BoolVal(false))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(false)))
+      }
+      "GTe positive" in {
+        val expression = App(GTe, BoolVal(true), BoolVal(true))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(true)))
+      }
+      "GTe negative" in {
+        val expression = App(GTe, BoolVal(false), BoolVal(true))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(false)))
+      }
+      "Gt positive" in {
+        val expression = App(Gt, BoolVal(true), BoolVal(false))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(true)))
+      }
+      "Gt negative" in {
+        val expression = App(Gt, BoolVal(true), BoolVal(true))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(false)))
+      }
+      "Ne positive" in {
+        val expression = App(Ne, BoolVal(true), BoolVal(false))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(true)))
+      }
+      "Ne negative" in {
+        val expression = App(Ne, BoolVal(true), BoolVal(true))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(false)))
+      }
+      "Eq positive" in {
+        val expression = App(Eq, BoolVal(true), BoolVal(true))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(true)))
+      }
+      "Eq negative" in {
+        val expression = App(Eq, BoolVal(true), BoolVal(false))
+
+        val answer: Answer = evaluate(expression)
+
+        answer should be (BooleanAnswer(Some(false)))
+      }
+    }
+  }
+}
