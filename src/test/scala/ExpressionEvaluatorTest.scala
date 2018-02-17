51f649cb0272147771814581eb508edf8fb2a3e2
rmathijn <rocco.mathijnandela@dhl.com>
2018-02-17 17:42:57 +0100

    Extends tests

--- src/test/scala/ExpressionEvaluatorTest.scala
+++ src/test/scala/ExpressionEvaluatorTest.scala
@@ -3,52 +3,304 @@ import org.scalatest.{ Matchers, WordSpec }
 
 class ExpressionEvaluatorTest extends WordSpec with Matchers {
   "The Expression evaluator" can {
-    "perform arithmetic expressions" in {
-      val expression = App(Add, IntAnswer(1), IntAnswer(2))
+    "do basic arithmetic operations" should {
+      "add" in {
+        val expression = App(Add, IntAnswer(5), IntAnswer(3))
 
-      val answer: Option[Answer] = evaluate(expression)
+        val answer: Option[Answer] = evaluate(expression)
 
-      answer should be (Some(IntAnswer(3)))
+        answer should be(Some(IntAnswer(8)))
+      }
+      "subtract" in {
+        val expression = App(Sub, IntAnswer(5), IntAnswer(3))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(IntAnswer(2)))
+      }
+      "multiply" in {
+        val expression = App(Mul, IntAnswer(5), IntAnswer(3))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(IntAnswer(15)))
+      }
+      "divide" in {
+        val expression = App(Div, IntAnswer(10), IntAnswer(5))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(IntAnswer(2)))
+      }
+      "return None when dividing by zero" in {
+        val expression = App(Div, IntAnswer(10), IntAnswer(0))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(None)
+      }
+      "throw an error for arithmetic operations on unsupported types" in {
+        val expression = App(Bla, BooleanAnswer(true), BooleanAnswer(true))
+
+        assertThrows[IllegalArgumentException](evaluate(expression))
+      }
     }
+    
+    "do comparison operations" should {
+      "on strings Lt positive" in {
+        val expression = App(Lt, StringAnswer("A"), StringAnswer("B"))
 
-    "perform nested arithmetics" in {
-      val expression1 = App(Add, IntAnswer(1), IntAnswer(2))
-      val expression2 = App(Add, expression1, App(Add, IntAnswer(3), IntAnswer(4)))
+        val answer: Option[Answer] = evaluate(expression)
 
-      val answer: Option[Answer] = evaluate(expression2)
+        answer should be(Some(BooleanAnswer(true)))
+      }
+      "on strings Lt negative" in {
+        val expression = App(Lt, StringAnswer("B"), StringAnswer("A"))
 
-      answer should be (Some(IntAnswer(10)))
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(false)))
+      }
+      "on strings LTe positive" in {
+        val expression = App(LTe, StringAnswer("A"), StringAnswer("A"))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(true)))
+      }
+      "on strings LTe negative" in {
+        val expression = App(LTe, StringAnswer("B"), StringAnswer("A"))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(false)))
+      }
+      "on strings GTe positive" in {
+        val expression = App(GTe, StringAnswer("B"), StringAnswer("B"))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(true)))
+      }
+      "on strings GTe negative" in {
+        val expression = App(GTe, StringAnswer("A"), StringAnswer("B"))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(false)))
+      }
+      "on strings Gt positive" in {
+        val expression = App(Gt, StringAnswer("B"), StringAnswer("A"))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(true)))
+      }
+      "on strings Gt negative" in {
+        val expression = App(Gt, StringAnswer("A"), StringAnswer("B"))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(false)))
+      }
+      "on strings Ne positive" in {
+        val expression = App(Ne, StringAnswer("A"), StringAnswer("B"))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(true)))
+      }
+      "on strings Ne negative" in {
+        val expression = App(Ne, StringAnswer("A"), StringAnswer("A"))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(false)))
+      }
+      "on strings Eq positive" in {
+        val expression = App(Eq, StringAnswer("A"), StringAnswer("A"))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(true)))
+      }
+      "on strings Eq negative" in {
+        val expression = App(Eq, StringAnswer("A"), StringAnswer("B"))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(false)))
+      }
+      "on integers Lt positive" in {
+        val expression = App(Lt, IntAnswer(1), IntAnswer(2))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(true)))
+      }
+      "on integers Lt negative" in {
+        val expression = App(Lt, IntAnswer(2), IntAnswer(1))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(false)))
+      }
+      "on integers LTe positive" in {
+        val expression = App(LTe, IntAnswer(1), IntAnswer(1))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(true)))
+      }
+      "on integers LTe negative" in {
+        val expression = App(LTe, IntAnswer(2), IntAnswer(1))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(false)))
+      }
+      "on integers GTe positive" in {
+        val expression = App(GTe, IntAnswer(5), IntAnswer(3))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(true)))
+      }
+      "on integers GTe negative" in {
+        val expression = App(GTe, IntAnswer(3), IntAnswer(5))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(false)))
+      }
+      "on integers Gt positive" in {
+        val expression = App(Gt, IntAnswer(5), IntAnswer(3))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(true)))
+      }
+      "on integers Gt negative" in {
+        val expression = App(Gt, IntAnswer(3), IntAnswer(5))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(false)))
+      }
+      "on integers Ne positive" in {
+        val expression = App(Ne, IntAnswer(5), IntAnswer(3))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(true)))
+      }
+      "on integers Ne negative" in {
+        val expression = App(Ne, IntAnswer(5), IntAnswer(5))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(false)))
+      }
+      "on integers Eq positive" in {
+        val expression = App(Eq, IntAnswer(5), IntAnswer(5))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(true)))
+      }
+      "on integers Eq negative" in {
+        val expression = App(Eq, IntAnswer(5), IntAnswer(3))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(false)))
+      }
+      "on booleans Lt positive" in {
+        val expression = App(Lt, BooleanAnswer(false), BooleanAnswer(true))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(true)))
+      }
+      "on booleans Lt negative" in {
+        val expression = App(Lt, BooleanAnswer(true), BooleanAnswer(false))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(false)))
+      }
+      "on booleans LTe positive" in {
+        val expression = App(LTe, BooleanAnswer(true), BooleanAnswer(true))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(true)))
+      }
+      "on booleans LTe negative" in {
+        val expression = App(LTe, BooleanAnswer(true), BooleanAnswer(false))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(false)))
+      }
+      "on booleans GTe positive" in {
+        val expression = App(GTe, BooleanAnswer(true), BooleanAnswer(true))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(true)))
+      }
+      "on booleans GTe negative" in {
+        val expression = App(GTe, BooleanAnswer(false), BooleanAnswer(true))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(false)))
+      }
+      "on booleans Gt positive" in {
+        val expression = App(Gt, BooleanAnswer(true), BooleanAnswer(false))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(true)))
+      }
+      "on booleans Gt negative" in {
+        val expression = App(Gt, BooleanAnswer(true), BooleanAnswer(true))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(false)))
+      }
+      "on booleans Ne positive" in {
+        val expression = App(Ne, BooleanAnswer(true), BooleanAnswer(false))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(true)))
+      }
+      "on booleans Ne negative" in {
+        val expression = App(Ne, BooleanAnswer(true), BooleanAnswer(true))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(false)))
+      }
+      "on booleans Eq positive" in {
+        val expression = App(Eq, BooleanAnswer(true), BooleanAnswer(true))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(true)))
+      }
+      "on booleans Eq negative" in {
+        val expression = App(Eq, BooleanAnswer(true), BooleanAnswer(false))
+
+        val answer: Option[Answer] = evaluate(expression)
+
+        answer should be(Some(BooleanAnswer(false)))
+      }
     }
-
-    "throw an error for unsupported operations" in {
-      val expression = App(Bla, IntAnswer(1), IntAnswer(2))
-
-      assertThrows[IllegalArgumentException](evaluate(expression))
-    }
-
-    "perform comparison" in {
-      val expression = App(Eq, IntAnswer(5), IntAnswer(2))
-
-      val answer: Option[Answer] = evaluate(expression)
-
-      answer should be (Some(BooleanAnswer(false)))
-    }
-
-    "perform boolean comparisons" in {
-      val expression = App(Eq, BooleanAnswer(true), BooleanAnswer(true))
-
-      val answer: Option[Answer] = evaluate(expression)
-
-      answer should be (Some(BooleanAnswer(true)))
-    }
-
-    "not mix answertypes" in {
-      val expression = App(Eq, BooleanAnswer(true), IntAnswer(5))
-
-      assertThrows[IllegalArgumentException](evaluate(expression))
-    }
-
-    "perform logical operations on booleans" should {
+    "perform logical operations" should {
       "Or positive" in {
         val expression = App(Or, BooleanAnswer(true), BooleanAnswer(false))
 
@@ -76,6 +328,18 @@ class ExpressionEvaluatorTest extends WordSpec with Matchers {
         val answer: Option[Answer] = evaluate(expression)
 
         answer should be(Some(BooleanAnswer(false)))
+      }
+    }
+    "do proper error handling" should {
+      "throw an error when evaluating mixed answertypes" in {
+        val expression = App(Eq, BooleanAnswer(true), IntAnswer(5))
+
+        assertThrows[IllegalArgumentException](evaluate(expression))
+      }
+      "throw an error for unsupported operations" in {
+        val expression = App(Bla, IntAnswer(1), IntAnswer(2))
+
+        assertThrows[IllegalArgumentException](evaluate(expression))
       }
     }
   }
